91051080f12ba1ef82dd3ff25c8b66eadd0b6dbf
Maxime Defaucheux <maxime.defaucheux@gmail.com>
2014-03-10 15:41:48 +0100

    Merge branch 'master' of https://github.com/Maxime06/Projet_RVIG
    Conflicts:
    	Assets/Interactions/interactions.unity

---
+++ Assets/Interactions/inter_fleches_center.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var speed : float = 20.0;
+function Start () {
+
+}
+
+function Update () {
+// rotation verticale haut
+if (Input.GetKey(KeyCode.UpArrow)) {
+	// point de rotation
+	// axe de roation
+	// angle
+	transform.RotateAround(Vector3.zero,Vector3.right,speed*Time.deltaTime);
+}
+// rotation verticale bas
+if (Input.GetKey(KeyCode.DownArrow)) {
+	transform.RotateAround(Vector3.zero,Vector3.right,-speed*Time.deltaTime);
+}
+
+// rotation horizontale gauche
+if (Input.GetKey(KeyCode.LeftArrow)) {
+	transform.RotateAround(Vector3.zero,Vector3.up,speed*Time.deltaTime);
+}
+
+// rotation verticale bas
+if (Input.GetKey(KeyCode.RightArrow)) {
+	transform.RotateAround(Vector3.zero,Vector3.up,-speed*Time.deltaTime);
+}
+}
